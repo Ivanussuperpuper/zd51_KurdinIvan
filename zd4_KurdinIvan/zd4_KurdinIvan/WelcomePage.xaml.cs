b6e45642a1fdf954b71db3ff910678890ddcc5ae
Ivanussuperpuper <ivankurdin84@gmail.com>
2025-06-22 14:51:23 +0500

    Initial commit

---
+++ zd4_KurdinIvan/zd4_KurdinIvan/WelcomePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd4_KurdinIvan
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage :ContentPage
+    {
+        public WelcomePage ()
+        {
+            InitializeComponent( );
+        }
+        private async void OnSignInClicked (object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(usernameEntry.Text))
+            {
+                await DisplayAlert("Ошибка", "введите тимя пользователя", "OK");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(passwordEntry.Text))
+            {
+                await DisplayAlert("Ошибка", "введите пароль", "OK");
+                return;
+            }
+
+            var mainPage = new MainPage( );
+            await Navigation.PushAsync(mainPage);
+            mainPage.SetWelcomeMessage(usernameEntry.Text);
+        }
+
+        private void OnForgotPasswordTapped (object sender, EventArgs e)
+        {
+            DisplayAlert("Информация", "Фунция сброса пароля", "OK");
+        }
+    }
+}
